2a99d8fbc241e9bb55603fd6051778b2f93dad0a
Tomáš Chroboček <tomas@chrobocek.com>
2020-11-29 14:34:14 +0100

    Added Qweree.Mongo to Qweree.Cdn.WebAPi

---
+++ src/Qweree.Cdn.WebApi.Test/Infrastructure/StoredObjectDescriptorRepositoryTest.cs
@@ -0,0 +1,42 @@
+using System.Net.Mime;
+using System.Threading.Tasks;
+using DeepEqual.Syntax;
+using Qweree.Cdn.WebApi.Infrastructure.Storage;
+using Qweree.Cdn.WebApi.Test.Fixture;
+using Qweree.Cdn.WebApi.Test.Fixture.Factories;
+using Qweree.Mongo;
+using Qweree.TestUtils.DeepEqual;
+using Xunit;
+
+namespace Qweree.Cdn.WebApi.Test.Infrastructure
+{
+    [Collection("Database collection")]
+    [Trait("Category", "Integration test")]
+    [Trait("Category", "Database test")]
+    public class StoredObjectDescriptorRepositoryTest
+    {
+        private readonly StoredObjectDescriptorRepository _repository;
+
+        public StoredObjectDescriptorRepositoryTest()
+        {
+            var context = new MongoContext(Settings.Database.ConnectionString, Settings.Database.DatabaseName);
+            _repository = new StoredObjectDescriptorRepository(context);
+            _repository.DeleteAllAsync()
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        [Fact]
+        public async Task TestInsertAndGet()
+        {
+            var descriptor = StoredObjectDescriptorFactory.CreateDefault("object", MediaTypeNames.Application.Octet, 0L);
+            await _repository.InsertAsync(descriptor);
+            var actual = await _repository.GetAsync(descriptor.Id);
+
+            actual.WithDeepEqual(descriptor)
+                .WithCustomComparison(new MillisecondDateTimeComparison())
+                .WithCustomComparison(new ImmutableArrayComparison())
+                .Assert();
+        }
+    }
+}
