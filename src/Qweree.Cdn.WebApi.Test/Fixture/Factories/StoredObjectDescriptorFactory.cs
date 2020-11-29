f1834fe6092008d8961b8ee4f8b1e78e95e5bf89
Tomáš Chroboček <tomas@chrobocek.com>
2020-11-29 15:29:17 +0100

    Added enhancements to Cdn tests

--- src/Qweree.Cdn.WebApi.Test/Fixture/Factories/StoredObjectDescriptorFactory.cs
+++ src/Qweree.Cdn.WebApi.Test/Fixture/Factories/StoredObjectDescriptorFactory.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Net.Mime;
 using Qweree.Cdn.WebApi.Domain.Storage;
 
 namespace Qweree.Cdn.WebApi.Test.Fixture.Factories
 {
     public class StoredObjectDescriptorFactory
     {
+        public static StoredObjectDescriptor CreateDefault()
+        {
+            return CreateDefault($"generated/{Guid.NewGuid()}", MediaTypeNames.Application.Octet, 0L);
+        }
+
         public static StoredObjectDescriptor CreateDefault(string name, string mediaType, long size)
         {
             return new StoredObjectDescriptor(Guid.NewGuid(), name.Split(", "), mediaType, size, DateTime.UtcNow,DateTime.UtcNow);
