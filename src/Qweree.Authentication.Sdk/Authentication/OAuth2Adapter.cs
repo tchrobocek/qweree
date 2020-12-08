3c09be3a55ed07c97ca985d98705b7e87f937d48
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-09 00:55:40 +0100

    Added error handler from  Qweree.Sdk.Http to Qweree.Authentication.Sdk

--- src/Qweree.Authentication.Sdk/Authentication/OAuth2Adapter.cs
+++ src/Qweree.Authentication.Sdk/Authentication/OAuth2Adapter.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Qweree.Authentication.Sdk.Exceptions;
-using Qweree.Sdk;
+using Qweree.Sdk.Http.Errors;
 using Qweree.Utils;
 
 namespace Qweree.Authentication.Sdk.Authentication
@@ -13,6 +12,7 @@ namespace Qweree.Authentication.Sdk.Authentication
     {
         private readonly Uri _authUri;
         private readonly HttpClient _httpClient;
+        private readonly IErrorHandler _errorResponseHandler = new QwereeErrorHandler();
 
         public OAuth2Adapter(Uri authUri, HttpClient httpClient)
         {
@@ -38,20 +38,10 @@ namespace Qweree.Authentication.Sdk.Authentication
             var response = await _httpClient.SendAsync(request, cancellationToken);
 
             if (!response.IsSuccessStatusCode)
-                await HandleResponseAsync(response, cancellationToken);
+                await _errorResponseHandler.HandleErrorResponse(response, cancellationToken);
 
             var tokenInfoDto = await response.Content.ReadAsObjectAsync<TokenInfoDto>(JsonUtils.SnakeCaseNamingPolicy, cancellationToken);
             return TokenInfoMapper.FromDto(tokenInfoDto!);
-        }
-
-        private async Task HandleResponseAsync(HttpResponseMessage response, CancellationToken cancellationToken)
-        {
-            var errorResponse = await response.Content.ReadAsObjectAsync<ErrorResponseDto>(cancellationToken);
-
-            if ((int)response.StatusCode >= 400 && (int)response.StatusCode < 500)
-                throw new ClientErrorException((int)response.StatusCode, errorResponse!);
-
-            throw new ServerErrorException((int)response.StatusCode, errorResponse!);
         }
     }
 }
