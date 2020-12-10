10550a4e2b6021f775949393b29b6e5c83a382a5
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-10 22:18:40 +0100

    Added material design

--- src/Qweree.QAdmin.WebApplication/src/app/app.module.ts
+++ src/Qweree.QAdmin.WebApplication/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SignInComponent} from './authentication/signin/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {SignInComponent} from './authentication/signin/sign-in.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
