b9de4591ba0a5092a74da513c76bcc766e6a3bbc
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-12 02:27:24 +0100

    Added version report

---
+++ src/Qweree.QAdmin.WebApplication/src/app/model/system/VersionReport.ts
@@ -0,0 +1,6 @@
+export class VersionReport {
+  constructor(
+    public version: string
+  ) {
+  }
+}
--- src/Qweree.QAdmin.WebApplication/src/app/services/system/system-adapter.service.ts
+++ src/Qweree.QAdmin.WebApplication/src/app/services/system/system-adapter.service.ts
@@ -1,6 +1,9 @@
 import {HttpClient} from '@angular/common/http';
 import {HealthReport} from '../../model/system/HealthReport';
 import {Observable} from 'rxjs';
+import {UriHelper} from '../UriHelper';
+import {VersionReport} from '../../model/system/VersionReport';
+import {catchError} from 'rxjs/operators';
 
 export class SystemAdapterService {
 
@@ -8,6 +11,12 @@ export class SystemAdapterService {
   }
 
   healthReport(): Observable<HealthReport> {
-    return this.httpClient.get<HealthReport>(this.uri + '/api/v1/_system/health');
+    return this.httpClient.get<HealthReport>(UriHelper.getUri(this.uri, '/api/v1/_system/health'))
+      .pipe(catchError(e => {throw e; }));
+  }
+
+  versionReport(): Observable<VersionReport> {
+    return this.httpClient.get<VersionReport>(UriHelper.getUri(this.uri, '/api/v1/_system/version'))
+      .pipe(catchError(e => {throw e; }));
   }
 }
