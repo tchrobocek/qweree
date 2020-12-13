4583265366b5f69ccfaaf4803f7dbb8e9df718a3
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-13 21:55:36 +0100

    Added cdn stats to its dashboard

--- src/Qweree.QAdmin.WebApplication/src/app/components/cdn/dashboard/dashboard.component.ts
+++ src/Qweree.QAdmin.WebApplication/src/app/components/cdn/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, SimpleChanges} from '@angular/core';
+import {ExplorerDirectory, ExplorerFile} from '../../../model/cdn/ExplorerObject';
+import {CdnAdapterService} from '../../../services/cdn/cdn-adapter.service';
 
 @Component({
   selector: 'app-cdn-dashboard',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public totalCount = 0;
+  public totalSize = 0;
+
+  constructor(
+    private cdnAdapter: CdnAdapterService
+  ) { }
 
   ngOnInit(): void {
+    this.cdnAdapter.explore('/')
+      .subscribe(objects => {
+        objects.forEach(o => {
+          const dir = (o as ExplorerDirectory);
+          if (dir.totalCount) {
+            this.totalSize += dir.totalSize;
+            this.totalCount += dir.totalCount;
+          }
+          const file = (o as ExplorerFile);
+          if (file.id) {
+            this.totalSize += file.size;
+            this.totalCount++;
+          }
+        });
+      });
   }
-
 }
