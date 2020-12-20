38eaaaf60f60ae328fe0f3b7a040461481f6ed16
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-20 17:46:29 +0100

    #12 Added PaginateAsync to IRepository

--- src/Qweree.Authentication.WebApi/Domain/Persistence/IRepository.cs
+++ src/Qweree.Authentication.WebApi/Domain/Persistence/IRepository.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Qweree.Mongo;
 
 namespace Qweree.Authentication.WebApi.Domain.Persistence
 {
     public interface IRepository<TEntityType>
     {
         Task<IEnumerable<TEntityType>> FindAsync(CancellationToken cancellationToken = new CancellationToken());
+        Task<Pagination<TEntityType>> PaginateAsync(int take, int skip, Dictionary<string, int> sort, CancellationToken cancellationToken = new CancellationToken());
         Task<long> CountAsync(CancellationToken cancellationToken = new CancellationToken());
         Task<TEntityType> GetAsync(Guid id, CancellationToken cancellationToken = new CancellationToken());
         Task InsertAsync(TEntityType document, CancellationToken cancellationToken = new CancellationToken());
