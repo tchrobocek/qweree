2c75b6da52b82fa9e7a9137c8604e97eec050796
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-20 17:11:31 +0100

    Added ToErrorActionResult extension to Response

--- src/Qweree.AspNet/Web/ResponseExtensions.cs
+++ src/Qweree.AspNet/Web/ResponseExtensions.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Qweree.AspNet.Application;
 using Qweree.Sdk;
 
@@ -12,6 +13,10 @@ namespace Qweree.AspNet.Web
             {
                 Errors = response.Errors.Select(e => new ErrorDto{Message = e.Message}).ToArray()
             };
+        }
+        public static IActionResult ToErrorActionResult(this Response response)
+        {
+            return new StatusCodeResult(response.Errors.Max(e => e.Code ?? 400));
         }
     }
 }
