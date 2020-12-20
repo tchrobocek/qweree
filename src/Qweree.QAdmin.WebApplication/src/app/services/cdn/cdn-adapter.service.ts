39d67b0378dc71affd508fd79cfa88021439debe
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-20 20:42:07 +0100

    Adjusted storage and explorer uris

--- src/Qweree.QAdmin.WebApplication/src/app/services/cdn/cdn-adapter.service.ts
+++ src/Qweree.QAdmin.WebApplication/src/app/services/cdn/cdn-adapter.service.ts
@@ -18,14 +18,14 @@ export class CdnAdapterService {
   }
 
   store(path: string, mediaType: string, file: ArrayBuffer): Observable<string> {
-    const uri = PathHelper.getPath(this.environment.getEnvironment().cdn.baseUri, '/api/v1/storage///');
+    const uri = PathHelper.getPath(this.environment.getEnvironment().cdn.baseUri, '/api/v1/storage/');
     const options = {headers: {'Content-Type': mediaType}};
     return this.httpClient.post<string>(PathHelper.getPath(uri, path), file, options)
       .pipe(catchError(e => {throw e; }));
   }
 
   explore(path: string): Observable<ExplorerObject[]> {
-    const explorerUri = PathHelper.getPath(this.environment.getEnvironment().cdn.baseUri, '/api/v1/explorer///');
+    const explorerUri = PathHelper.getPath(this.environment.getEnvironment().cdn.baseUri, '/api/v1/explorer/');
     return this.httpClient.get<ExplorerObject[]>(PathHelper.getPath(explorerUri, path))
       .pipe(catchError(e => {throw e; }));
   }
