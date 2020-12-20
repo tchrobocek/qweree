03f21a5ead6a9deb1e1e70c9ae3ee3bbf7a06a72
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-20 16:21:43 +0100

    #10 Changed role UserUpdate for AUTH_USERS_CREATE

--- scripts/mongo-init.js
+++ scripts/mongo-init.js
@@ -6,7 +6,7 @@ db.users.insert({
     "Password": "$2a$11$BwiSQi56B5UkW4SywF07g.jB3AA0GzHD1YsCWPLMzUlJi9UQUborC",
     "ContactEmail": "admin@admin.com",
     "FullName": "Admin Adminowitch",
-    "Roles": ["UserUpdate"],
+    "Roles": ["AUTH_USERS_CREATE"],
     "CreatedAt": ISODate("1970-01-01"),
     "ModifiedAt": ISODate("1970-01-01")
 });
