d098695310697f5c09712646663ccad9ca8c742d
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-20 18:30:45 +0100

    Added paginate users action

--- src/Qweree.Authentication.WebApi.Test/Fixture/Factories/UserFactory.cs
+++ src/Qweree.Authentication.WebApi.Test/Fixture/Factories/UserFactory.cs
@@ -6,9 +6,9 @@ namespace Qweree.Authentication.WebApi.Test.Fixture.Factories
     public static class UserFactory
     {
         public const string Password = "pwd";
-        public static User CreateDefault()
+        public static User CreateDefault(string username = "user")
         {
-            return new User(Guid.NewGuid(), "user", "User Userov", "user@example.com", BCrypt.Net.BCrypt.HashPassword(Password), Array.Empty<string>(), DateTime.UtcNow, DateTime.UtcNow);
+            return new User(Guid.NewGuid(), username, "User Userov", "user@example.com", BCrypt.Net.BCrypt.HashPassword(Password), Array.Empty<string>(), DateTime.UtcNow, DateTime.UtcNow);
         }
 
         public static User CreateAdmin()
--- src/Qweree.Authentication.WebApi/Domain/Persistence/IRepository.cs
+++ src/Qweree.Authentication.WebApi/Domain/Persistence/IRepository.cs
@@ -9,7 +9,7 @@ namespace Qweree.Authentication.WebApi.Domain.Persistence
     public interface IRepository<TEntityType>
     {
         Task<IEnumerable<TEntityType>> FindAsync(CancellationToken cancellationToken = new CancellationToken());
-        Task<Pagination<TEntityType>> PaginateAsync(int take, int skip, Dictionary<string, int> sort, CancellationToken cancellationToken = new CancellationToken());
+        Task<Pagination<TEntityType>> PaginateAsync(int skip, int take, Dictionary<string, int> sort, CancellationToken cancellationToken = new CancellationToken());
         Task<long> CountAsync(CancellationToken cancellationToken = new CancellationToken());
         Task<TEntityType> GetAsync(Guid id, CancellationToken cancellationToken = new CancellationToken());
         Task InsertAsync(TEntityType document, CancellationToken cancellationToken = new CancellationToken());
