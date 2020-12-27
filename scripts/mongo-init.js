97c918b750b9995038bcf43cbf8775777b401dbf
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-27 18:34:47 +0100

    Added all roles to admin user in mongo script

--- scripts/mongo-init.js
+++ scripts/mongo-init.js
@@ -6,7 +6,16 @@ db.users.insert({
     "Password": "$2a$11$BwiSQi56B5UkW4SywF07g.jB3AA0GzHD1YsCWPLMzUlJi9UQUborC",
     "ContactEmail": "admin@admin.com",
     "FullName": "Admin Adminowitch",
-    "Roles": ["AUTH_USERS_CREATE", "AUTH_USERS_READ", "AUTH_USERS_DELETE", "AUTH_USERS_READ_PERSONAL_DETAIL"],
+    "Roles": [
+        "AUTH_MANAGE",
+        "AUTH_USERS_CREATE",
+        "AUTH_USERS_READ",
+        "AUTH_USERS_DELETE",
+        "AUTH_USERS_READ_PERSONAL_DETAIL",
+        "CDN_MANAGE",
+        "CDN_EXPLORE",
+        "CDN_STORAGE_STORE",
+    ],
     "CreatedAt": ISODate("1970-01-01"),
     "ModifiedAt": ISODate("1970-01-01")
 });
