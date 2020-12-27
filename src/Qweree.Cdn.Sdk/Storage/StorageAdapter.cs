0d9b27c2595a6fc1b1ec2d89f470b7e16198c4c6
Tomáš Chroboček <tomas@chrobocek.com>
2020-12-27 23:02:19 +0100

    Added dotnet github workflow

--- src/Qweree.Cdn.Sdk/Storage/StorageAdapter.cs
+++ src/Qweree.Cdn.Sdk/Storage/StorageAdapter.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Net.Http.Headers;
 using Qweree.Sdk.Http.Errors;
 using Qweree.Utils;
 
@@ -30,7 +29,7 @@ namespace Qweree.Cdn.Sdk.Storage
                 {
                     Headers =
                     {
-                        {HeaderNames.ContentType, mediaType}
+                        {"Content-Type", mediaType}
                     }
                 }
             };
