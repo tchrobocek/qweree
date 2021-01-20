e5f5571b214e91aecb1664df19f647af6ecb187b
Tomáš Chroboček <tomas@chrobocek.com>
2021-01-20 01:12:52 +0100

    Changed package name and added nginx in Qweree.Qwill.WebApplication

--- src/Qweree.Qwill.WebApplication/e2e/src/app.e2e-spec.ts
+++ src/Qweree.Qwill.WebApplication/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('qweree-qwill app is running!');
+    expect(page.getTitleText()).toEqual('qwill app is running!');
   });
 
   afterEach(async () => {
