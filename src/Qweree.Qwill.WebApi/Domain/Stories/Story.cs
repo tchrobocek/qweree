63b976e82b9b449209bdfa0f414da04fc07687eb
Tomáš Chroboček <tomas@chrobocek.com>
2021-01-26 23:43:12 +0100

    Added story and publication service

--- src/Qweree.Qwill.WebApi/Domain/Stories/Story.cs
+++ src/Qweree.Qwill.WebApi/Domain/Stories/Story.cs
@@ -1,15 +1,31 @@
+using System;
+using System.Collections.Immutable;
+using Qweree.Qwill.WebApi.Domain.Publishers;
+
 namespace Qweree.Qwill.WebApi.Domain.Stories
 {
     public class Story
     {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string LeadingText { get; set; }
-        public string LeadingImage { get; set; }
-        public List<string> Pages { get; set; } = new();
-        public string Language { get; set; }
-        public Author Author { get; set; }
-        public Channel Channel { get; set; }
-        public DateTime PublishedAt { get; set; }
+        public Story(Guid id, string title, string leadParagraph, string leadImage, string language, Author author,
+            DateTime publishedAt, ImmutableArray<string> pages)
+        {
+            Id = id;
+            Title = title;
+            LeadParagraph = leadParagraph;
+            LeadImage = leadImage;
+            Language = language;
+            Author = author;
+            PublishedAt = publishedAt;
+            Pages = pages;
+        }
+
+        public Guid Id { get; }
+        public string Title { get; }
+        public string LeadParagraph { get; }
+        public string LeadImage { get; }
+        public ImmutableArray<string> Pages { get; }
+        public string Language { get; }
+        public Author Author { get; }
+        public DateTime PublishedAt { get; }
     }
 }
