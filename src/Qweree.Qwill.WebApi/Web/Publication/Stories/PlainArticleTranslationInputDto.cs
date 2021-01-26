9ceede5a94b92c173dc1e2e6b4475507d8e71677
Tomáš Chroboček <tomas@chrobocek.com>
2021-01-26 22:18:46 +0100

    Changed plain article tranlanslation input pages to content

--- src/Qweree.Qwill.WebApi/Web/Publication/Stories/PlainArticleTranslationInputDto.cs
+++ src/Qweree.Qwill.WebApi/Web/Publication/Stories/PlainArticleTranslationInputDto.cs
@@ -1,12 +1,10 @@
-using System.Collections.Generic;
-
 namespace Qweree.Qwill.WebApi.Web.Publication.Stories
 {
     public class PlainArticleTranslationInputDto
     {
         public string? Language { get; set; }
         public string? Title { get; set; }
-        public List<string> Pages { get; set; } = new();
+        public string? Content { get; set; }
         public string? LeadParagraph { get; set; }
         public string? HeaderImageSlug { get; set; }
     }
