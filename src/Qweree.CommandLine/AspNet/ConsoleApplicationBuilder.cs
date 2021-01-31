707582f632b715c37263e5786368433c8e7b070f
Tomáš Chroboček <tomas@chrobocek.com>
2021-01-31 23:02:43 +0100

    Added Qweree.CommandLine project

---
+++ src/Qweree.CommandLine/AspNet/ConsoleApplicationBuilder.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Qweree.CommandLine.AspNet
+{
+    public delegate Task RequestDelegate(ConsoleContext context, CancellationToken cancellationToken = new());
+
+    public class ConsoleContext
+    {
+        public string[] Args { get; set; } = Array.Empty<string>();
+        public int ReturnCode { get; set; } = 0;
+    }
+    public interface IMiddleware
+    {
+        Task NextAsync(ConsoleContext context, RequestDelegate? next, CancellationToken cancellationToken = new());
+    }
+
+    public static class ConsoleApplicationBuilderExtensions
+    {
+        public static void UseMiddleware(this ConsoleApplicationBuilder app, IMiddleware middleware)
+        {
+            app.Use(next =>
+            {
+                return async (context, token) =>
+                {
+                    await middleware.NextAsync(context, next, token);
+                };
+            });
+        }
+    }
+
+    public class ConsoleApplicationBuilder
+    {
+        private readonly List<Func<RequestDelegate?, RequestDelegate>> _delegates = new();
+
+        public ConsoleApplicationBuilder(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public IServiceProvider ServiceProvider { get; }
+
+        public void Use(Func<RequestDelegate?, RequestDelegate> func)
+        {
+            _delegates.Add(func);
+        }
+
+        public RequestDelegate Build()
+        {
+            return Application;
+        }
+
+        private async Task Application(ConsoleContext context, CancellationToken cancellationToken = new())
+        {
+            RequestDelegate? next = null;
+            for (var i = _delegates.Count - 1; i >= 0; i--)
+            {
+                var func = _delegates[i];
+                next = func(next);
+            }
+
+            if (next != null)
+            {
+                await next(context, cancellationToken);
+            }
+        }
+    }
+}
