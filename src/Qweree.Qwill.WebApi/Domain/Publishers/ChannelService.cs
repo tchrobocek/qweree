5ca064e4b5bb2a00767910cb54b2fb5619f1aa12
Tomáš Chroboček <tomas@chrobocek.com>
2021-02-18 22:07:58 +0100

    Added creating channel

--- src/Qweree.Qwill.WebApi/Domain/Publishers/ChannelService.cs
+++ src/Qweree.Qwill.WebApi/Domain/Publishers/ChannelService.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Qweree.AspNet.Application;
 using Qweree.AspNet.Session;
+using Qweree.Utils;
+using Qweree.Validator;
 
 namespace Qweree.Qwill.WebApi.Domain.Publishers
 {
@@ -9,11 +14,16 @@ namespace Qweree.Qwill.WebApi.Domain.Publishers
     {
         private readonly IChannelRepository _channelRepository;
         private readonly ISessionStorage _sessionStorage;
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly IValidator _validator;
 
-        public ChannelService(IChannelRepository channelRepository, ISessionStorage sessionStorage)
+        public ChannelService(IChannelRepository channelRepository, ISessionStorage sessionStorage,
+            IDateTimeProvider dateTimeProvider, IValidator validator)
         {
             _channelRepository = channelRepository;
             _sessionStorage = sessionStorage;
+            _dateTimeProvider = dateTimeProvider;
+            _validator = validator;
         }
 
         public async Task<CollectionResponse<Channel>> GetOwnChannelsAsync(CancellationToken cancellationToken = new())
@@ -21,6 +31,23 @@ namespace Qweree.Qwill.WebApi.Domain.Publishers
             var user = _sessionStorage.CurrentUser;
             var channels = await _channelRepository.FindUserAuthorAsync(user.Id, cancellationToken);
             return Response.Ok(channels);
+        }
+
+        public async Task<Response<Channel>> CreateAsync(ChannelCreateInput input,
+            CancellationToken cancellationToken = new())
+        {
+            var user = _sessionStorage.CurrentUser;
+
+            var result = await _validator.ValidateAsync(input, cancellationToken);
+            if (result.HasFailed)
+            {
+                return Response.Fail<Channel>(result.Errors.Select(e => $"{e.Path} - {e.Message}"));
+            }
+
+            var channel = new Channel(Guid.NewGuid(), input.ChannelName, new[] {user.Id}.ToImmutableArray(), user.Id,
+                _dateTimeProvider.UtcNow, _dateTimeProvider.UtcNow);
+            await _channelRepository.InsertAsync(channel, cancellationToken);
+            return Response.Ok(channel);
         }
     }
 }
--- src/Qweree.Qwill.WebApi/Infrastructure/ValidationMap.cs
+++ src/Qweree.Qwill.WebApi/Infrastructure/ValidationMap.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using Qweree.Qwill.WebApi.Domain.Publishers;
 using Qweree.Qwill.WebApi.Domain.Stories;
 using Qweree.Validator.Constraints;
 using Qweree.Validator.ModelValidation.Static;
@@ -17,6 +18,12 @@ namespace Qweree.Qwill.WebApi.Infrastructure
                     .AddConstraint(new MinLengthConstraint(3, "Title has to be at least 3 chars long."));
                 model.AddProperty(e => e.LeadParagraph)
                     .AddConstraint(new MinLengthConstraint(3, "Lead paragraph has to be at least 3 chars long."));
+            });
+
+            builder.AddModel<ChannelCreateInput>(model =>
+            {
+                model.AddProperty(p => p.ChannelName)
+                    .AddConstraint(new MinLengthConstraint(3, "Channel name has to be at least 3 chars long."));
             });
         }
     }
