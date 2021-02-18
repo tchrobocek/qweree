8b7dffd566c20f055044052bbe5d12576c9a6797
Tomáš Chroboček <tomas@chrobocek.com>
2021-02-18 21:17:32 +0100

    Removed not used ChannelDto

--- src/Qweree.Qwill.WebApi/Web/Publication/Publishers/ChannelDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Qweree.Qwill.WebApi.Web.Publication.Publishers
-{
-    public class ChannelDto
-    {
-        public string? Id { get; set; }
-        public string? Name { get; set; }
-    }
-}
