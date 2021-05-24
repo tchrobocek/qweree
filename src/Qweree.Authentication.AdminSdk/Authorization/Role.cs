0989a2d74bd0acabca5239e87dc36500019e2232
Tomáš Chroboček <tomas@chrobocek.com>
2021-05-24 23:07:01 +0200

    #39 Added sdk role and mapper

---
+++ src/Qweree.Authentication.AdminSdk/Authorization/Role.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Immutable;
+
+namespace Qweree.Authentication.AdminSdk.Authorization
+{
+    public class UserRole
+    {
+        public UserRole(Guid id, string key, string label, string description, ImmutableArray<UserRole> items, bool isGroup, DateTime createdAt, DateTime modifiedAt, ImmutableArray<UserRole> effectiveRoles)
+        {
+            Id = id;
+            Key = key;
+            Label = label;
+            Description = description;
+            Items = items;
+            IsGroup = isGroup;
+            CreatedAt = createdAt;
+            ModifiedAt = modifiedAt;
+            EffectiveRoles = effectiveRoles;
+        }
+
+        public Guid Id { get; }
+        public string Key { get; }
+        public string Label { get; }
+        public string Description { get; }
+        public ImmutableArray<UserRole> Items { get; }
+        public bool IsGroup { get; }
+        public DateTime CreatedAt { get; }
+        public DateTime ModifiedAt { get; }
+        public ImmutableArray<UserRole> EffectiveRoles { get; }
+    }
+
+    public class ClientRole
+    {
+        public ClientRole(Guid id, string key, string label, string description, ImmutableArray<ClientRole> items, bool isGroup, DateTime createdAt, DateTime modifiedAt, ImmutableArray<ClientRole> effectiveRoles)
+        {
+            Id = id;
+            Key = key;
+            Label = label;
+            Description = description;
+            Items = items;
+            IsGroup = isGroup;
+            CreatedAt = createdAt;
+            ModifiedAt = modifiedAt;
+            EffectiveRoles = effectiveRoles;
+        }
+
+        public Guid Id { get; }
+        public string Key { get; }
+        public string Label { get; }
+        public string Description { get; }
+        public ImmutableArray<ClientRole> Items { get; }
+        public bool IsGroup { get; }
+        public DateTime CreatedAt { get; }
+        public DateTime ModifiedAt { get; }
+        public ImmutableArray<ClientRole> EffectiveRoles { get; }
+    }
+}
--- src/Qweree.Authentication.WebApi/Domain/SdkMapperService.cs
+++ src/Qweree.Authentication.WebApi/Domain/SdkMapperService.cs
@@ -1,20 +1,29 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Qweree.Authentication.AdminSdk.Identity.Clients;
+using Qweree.Authentication.WebApi.Domain.Authorization;
 using Qweree.Authentication.WebApi.Domain.Identity;
 using Client = Qweree.Authentication.WebApi.Domain.Identity.Client;
 using SdkClient = Qweree.Authentication.AdminSdk.Identity.Clients.Client;
 using SdkUser = Qweree.Authentication.AdminSdk.Identity.Users.User;
+using SdkUserRole = Qweree.Authentication.AdminSdk.Authorization.UserRole;
+using SdkClientRole = Qweree.Authentication.AdminSdk.Authorization.ClientRole;
 
 namespace Qweree.Authentication.WebApi.Domain
 {
     public class SdkMapperService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IUserRoleRepository _userRoleRepository;
+        private readonly IClientRoleRepository _clientRoleRepository;
 
-        public SdkMapperService(IUserRepository userRepository)
+        public SdkMapperService(IUserRepository userRepository, IUserRoleRepository userRoleRepository, IClientRoleRepository clientRoleRepository)
         {
             _userRepository = userRepository;
+            _userRoleRepository = userRoleRepository;
+            _clientRoleRepository = clientRoleRepository;
         }
 
         public SdkUser MapUser(User user)
@@ -32,6 +41,44 @@ namespace Qweree.Authentication.WebApi.Domain
         {
             var owner = await _userRepository.GetAsync(client.OwnerId, cancellationToken);
             return new(client.Id, client.ClientId, client.ApplicationName, client.Origin, MapUser(owner), client.CreatedAt, client.ModifiedAt);
+        }
+
+        public async Task<SdkUserRole> MapUserRoleAsync(UserRole role, CancellationToken cancellationToken = new())
+        {
+            var items = new List<SdkUserRole>();
+
+            if (role.IsGroup)
+            {
+                foreach (var itemId in role.Items)
+                {
+                    var item = await _userRoleRepository.GetAsync(itemId, cancellationToken);
+                    var sdkItem = await MapUserRoleAsync(item, cancellationToken);
+
+                    items.Add(sdkItem);
+                }
+            }
+
+            return new SdkUserRole(role.Id, role.Key, role.Label, role.Description, items.ToImmutableArray(),
+                role.IsGroup, role.CreatedAt, role.ModifiedAt, ImmutableArray<SdkUserRole>.Empty);
+        }
+
+        public async Task<SdkClientRole> MapClientRoleAsync(ClientRole role, CancellationToken cancellationToken = new())
+        {
+            var items = new List<SdkClientRole>();
+
+            if (role.IsGroup)
+            {
+                foreach (var itemId in role.Items)
+                {
+                    var item = await _clientRoleRepository.GetAsync(itemId, cancellationToken);
+                    var sdkItem = await MapClientRoleAsync(item, cancellationToken);
+
+                    items.Add(sdkItem);
+                }
+            }
+
+            return new SdkClientRole(role.Id, role.Key, role.Label, role.Description, items.ToImmutableArray(),
+                role.IsGroup, role.CreatedAt, role.ModifiedAt, ImmutableArray<SdkClientRole>.Empty);
         }
     }
 }
