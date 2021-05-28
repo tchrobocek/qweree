21d3dfcd29527aafd3354ed6cd203d82ce46de5a
Tomáš Chroboček <tomas@chrobocek.com>
2021-05-28 19:16:30 +0200

    Removed not used class

--- src/Qweree.Authentication.WebApi/Web/Authentication/AuthenticationInputDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Qweree.Authentication.WebApi.Web.Authentication
-{
-    public class AuthenticationInputDto
-    {
-        public string? GrantType { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-    }
-}
