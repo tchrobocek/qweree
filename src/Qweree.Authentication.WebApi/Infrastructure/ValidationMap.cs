dd3d0e309e23e174a765ca037db45b7876fdfebe
Tomáš Chroboček <tomas@chrobocek.com>
2021-05-28 21:43:16 +0200

    Added client create validationn

--- src/Qweree.Authentication.WebApi/Infrastructure/ValidationMap.cs
+++ src/Qweree.Authentication.WebApi/Infrastructure/ValidationMap.cs
@@ -1,4 +1,5 @@
 using Qweree.Authentication.AdminSdk.Authorization.Roles;
+using Qweree.Authentication.AdminSdk.Identity.Clients;
 using Qweree.Authentication.AdminSdk.Identity.Users;
 using Qweree.Authentication.WebApi.Infrastructure.Authorization.Roles;
 using Qweree.Authentication.WebApi.Infrastructure.Identity;
@@ -27,6 +28,27 @@ namespace Qweree.Authentication.WebApi.Infrastructure
                     .AddConstraint(new PasswordConstraint());
                 c.AddProperty(p => p.Roles)
                     .AddConstraint(new ExistsConstraint(typeof(UserRoleRepository)));
+            });
+
+            builder.AddModel<ClientCreateInput>(c =>
+            {
+                c.AddProperty(p => p.ClientId)
+                    .AddConstraint(new UniqueConstraint(typeof(ClientRepository)))
+                    .AddConstraint(new NotEmptyConstraint("Client id cannot be empty."))
+                    .AddConstraint(new MinLengthConstraint(3, "Client id has to have 3 or more characters."))
+                    .AddConstraint(new MaxLengthConstraint(255, "Client id has to have up to 255 characters."));
+                c.AddProperty(p => p.Origin)
+                    .AddConstraint(new MaxLengthConstraint(255, "Origin has to have up to 255 characters."));
+                c.AddProperty(p => p.ApplicationName)
+                    .AddConstraint(new NotEmptyConstraint("Application name cannot be empty."))
+                    .AddConstraint(new MinLengthConstraint(3, "Application name has to have 3 or more characters."))
+                    .AddConstraint(new MaxLengthConstraint(255, "Application name has to have up to 255 characters."));
+                c.AddProperty(p => p.Roles)
+                    .AddConstraint(new ExistsConstraint(typeof(ClientRoleRepository)));
+                c.AddProperty(p => p.ClientSecret)
+                    .AddConstraint(new NotEmptyConstraint("Client secret cannot be empty."));
+                c.AddProperty(p => p.OwnerId)
+                    .AddConstraint(new ExistsConstraint(typeof(ClientRoleRepository)));
             });
 
             builder.AddModel<UserRoleCreateInput>(c =>
