3f88b3c521ea22e35d573af60c184253a60e03c6
Tomáš Chroboček <tomas@chrobocek.com>
2021-07-22 19:01:51 +0200

    Enhanced ImmutableArrayComparison

--- src/Qweree.TestUtils/DeepEqual/ImmutableArrayComparison.cs
+++ src/Qweree.TestUtils/DeepEqual/ImmutableArrayComparison.cs
@@ -1,28 +1,32 @@
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using DeepEqual;
 using DeepEqual.Syntax;
+using Xunit;
 
 namespace Qweree.TestUtils.DeepEqual
 {
     public class ImmutableArrayComparison : IComparison
     {
+        private readonly bool _withDeepEqual;
+
+        public ImmutableArrayComparison(bool withDeepEqual = true)
+        {
+            _withDeepEqual = withDeepEqual;
+        }
+
         public bool CanCompare(Type type1, Type type2)
         {
-            if (typeof(IEnumerable<object>).IsAssignableFrom(type1) &&
-                typeof(IEnumerable<object>).IsAssignableFrom(type2))
-                if (type1.Name == "ImmutableArray`1" || type2.Name == "ImmutableArray`1")
-                    return true;
-
-            return false;
+            return (type1.Name == "ImmutableArray`1" && type1.Namespace == "System.Collections.Immutable") ||
+                   (type2.Name == "ImmutableArray`1" && type2.Namespace == "System.Collections.Immutable");
         }
 
         public (ComparisonResult result, IComparisonContext context) Compare(IComparisonContext context, object value1,
             object value2)
         {
-            var array1 = ((IEnumerable<object>) value1).ToArray();
-            var array2 = ((IEnumerable<object>) value2).ToArray();
+            var array1 = ((IEnumerable) value1).Cast<object>().ToArray();
+            var array2 = ((IEnumerable) value2).Cast<object>().ToArray();
 
             if (array1.Length != array2.Length)
             {
@@ -31,11 +35,13 @@ namespace Qweree.TestUtils.DeepEqual
                 return (ComparisonResult.Fail, context);
             }
 
-            for (var i = 0; i < array1.Length; i++)
+            if (_withDeepEqual)
             {
-                var val1 = array1[i];
-                var val2 = array2[i];
-                val1.ShouldDeepEqual(val2);
+                array1.ShouldDeepEqual(array2);
+            }
+            else
+            {
+                Assert.Equal(array1, array2);
             }
 
             return (ComparisonResult.Pass, context);
