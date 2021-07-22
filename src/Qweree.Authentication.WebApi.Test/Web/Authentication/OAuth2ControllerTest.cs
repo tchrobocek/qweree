e81d8cf574bed877d22f2a9d6e5ed4934dc540dd
Tomáš Chroboček <tomas@chrobocek.com>
2021-07-22 22:12:49 +0200

    Added test for refresh grant

--- src/Qweree.Authentication.WebApi.Test/Web/Authentication/OAuth2ControllerTest.cs
+++ src/Qweree.Authentication.WebApi.Test/Web/Authentication/OAuth2ControllerTest.cs
@@ -4,12 +4,14 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
+using Qweree.Authentication.Sdk.Tokens;
 using Qweree.Authentication.WebApi.Domain.Authentication;
 using Qweree.Authentication.WebApi.Domain.Identity;
 using Qweree.Authentication.WebApi.Infrastructure.Identity;
 using Qweree.Authentication.WebApi.Infrastructure.Security;
 using Qweree.Authentication.WebApi.Test.Fixture;
 using Qweree.Authentication.WebApi.Test.Fixture.Factories;
+using Qweree.Utils;
 using Xunit;
 
 namespace Qweree.Authentication.WebApi.Test.Web.Authentication
@@ -96,6 +98,69 @@ namespace Qweree.Authentication.WebApi.Test.Web.Authentication
 
             var response = await _client.SendAsync(message);
             response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task TestRefresh()
+        {
+            var user = UserFactory.CreateDefault();
+            await _userRepository.InsertAsync(user);
+            var client = ClientFactory.CreateDefault(user.Id);
+            await _clientRepository.InsertAsync(client);
+
+            var refreshToken = "";
+
+            {
+                var input = new[]
+                {
+                    new KeyValuePair<string?, string?>("grant_type", "password"),
+                    new KeyValuePair<string?, string?>("username", user.Username),
+                    new KeyValuePair<string?, string?>("password", user.Password),
+                };
+
+                var authHeaderEncoder = new AuthorizationHeaderEncoder();
+                var authHeader = authHeaderEncoder.Encode(new ClientCredentials(client.ClientId, client.ClientSecret));
+                var request = new FormUrlEncodedContent(input);
+                var message = new HttpRequestMessage(HttpMethod.Post, "/api/oauth2/auth")
+                {
+                    Content = request,
+                    Headers =
+                    {
+                        {HeaderNames.Authorization, new[] {$"Basic {authHeader}"}}
+                    }
+                };
+
+                var response = await _client.SendAsync(message);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                var tokenInfo = JsonUtils.Deserialize<TokenInfoDto>(content, JsonUtils.SnakeCaseNamingPolicy);
+
+                refreshToken = tokenInfo!.RefreshToken;
+            }
+
+            {
+                var input = new[]
+                {
+                    new KeyValuePair<string?, string?>("grant_type", "refresh_token"),
+                    new KeyValuePair<string?, string?>("refresh_token", refreshToken),
+                };
+
+                var authHeaderEncoder = new AuthorizationHeaderEncoder();
+                var authHeader = authHeaderEncoder.Encode(new ClientCredentials(client.ClientId, client.ClientSecret));
+                var request = new FormUrlEncodedContent(input);
+                var message = new HttpRequestMessage(HttpMethod.Post, "/api/oauth2/auth")
+                {
+                    Content = request,
+                    Headers =
+                    {
+                        {HeaderNames.Authorization, new[] {$"Basic {authHeader}"}}
+                    }
+                };
+
+                var response = await _client.SendAsync(message);
+                response.EnsureSuccessStatusCode();
+            }
         }
     }
 }
