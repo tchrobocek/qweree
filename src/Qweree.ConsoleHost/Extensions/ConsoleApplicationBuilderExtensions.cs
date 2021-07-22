b0bdd2349ab81bc7f8493be643aa5b1430b8210a
Tomáš Chroboček <tomas@chrobocek.com>
2021-07-22 22:49:02 +0200

    Removed customized run app from Host

---
+++ src/Qweree.ConsoleHost/Extensions/ConsoleApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Qweree.ConsoleHost.Extensions
+{
+    public interface IMiddleware
+    {
+        Task NextAsync(ConsoleContext context, RequestDelegate? next, CancellationToken cancellationToken = new());
+    }
+
+    public static class ConsoleApplicationBuilderExtensions
+    {
+        public static void UseMiddleware<TMiddlewareType>(this ConsoleApplicationBuilder app) where TMiddlewareType : IMiddleware
+        {
+            app.Use(next =>
+            {
+                return async (context, token) =>
+                {
+                    var middleware = app.ServiceProvider.GetRequiredService<TMiddlewareType>();
+                    await middleware.NextAsync(context, next, token);
+                };
+            });
+        }
+    }
+}
--- src/Qweree.ConsoleHost/Host.cs
+++ src/Qweree.ConsoleHost/Host.cs
@@ -13,12 +13,10 @@ namespace Qweree.ConsoleHost
         {
             ConfigureAction = DefaultConfigureAction;
             ConfigureServicesAction = DefaultConfigureServicesAction;
-            RunApplicationAction = DefaultRunApplicationAction;
         }
 
         public Action<IServiceCollection> ConfigureServicesAction { get; set; }
         public Action<ConsoleApplicationBuilder> ConfigureAction { get; set; }
-        public Func<string[], Func<IServiceProvider, RequestDelegate>, CancellationToken, Task<int>> RunApplicationAction { get; set; }
 
         public ServiceProvider ServiceProvider => _serviceProvider ?? throw new InvalidOperationException("host is not built yet.");
 
@@ -30,7 +28,9 @@ namespace Qweree.ConsoleHost
         {
         }
 
-        private async Task<int> DefaultRunApplicationAction(string[] args, Func<IServiceProvider, RequestDelegate> buildApplicationFunction, CancellationToken cancellationToken = new())
+        private async Task<int> RunApplicationAsync(string[] args,
+            Func<IServiceProvider, RequestDelegate> buildApplicationFunction,
+            CancellationToken cancellationToken = new())
         {
             using var scope = ServiceProvider.CreateScope();
             var next = buildApplicationFunction(scope.ServiceProvider);
@@ -42,6 +42,13 @@ namespace Qweree.ConsoleHost
             return context.ReturnCode ?? -1;
         }
 
+        private RequestDelegate BuildApplication(IServiceProvider serviceProvider)
+        {
+            var builder = new ConsoleApplicationBuilder(serviceProvider);
+            ConfigureAction.Invoke(builder);
+            return builder.Build();
+        }
+
         public Host Build()
         {
             var collection = new ServiceCollection();
@@ -50,21 +57,14 @@ namespace Qweree.ConsoleHost
             return this;
         }
 
-        public async Task<int> RunAsync(string[] args)
+        public async Task<int> RunAsync(string[] args, CancellationToken cancellationToken = new())
         {
-            var result = await RunApplicationAction(args, BuildApplication, new CancellationToken());
+            var result = await RunApplicationAsync(args, BuildApplication, cancellationToken);
 
             if (_serviceProvider != null)
                 await _serviceProvider.DisposeAsync();
 
             return result;
-        }
-
-        private RequestDelegate BuildApplication(IServiceProvider serviceProvider)
-        {
-            var builder = new ConsoleApplicationBuilder(serviceProvider);
-            ConfigureAction.Invoke(builder);
-            return builder.Build();
         }
     }
 }
