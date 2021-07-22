d0abc336a2427a7a2c9717ecc240dc4ed442ddb8
Tomáš Chroboček <tomas@chrobocek.com>
2021-07-22 19:31:29 +0200

    Added authorization header encoder #45

---
+++ src/Qweree.Authentication.WebApi.Test/Infrastructure/Security/AuthorizationHeaderEncoderTest.cs
@@ -0,0 +1,21 @@
+using DeepEqual.Syntax;
+using Qweree.Authentication.WebApi.Domain.Authentication;
+using Qweree.Authentication.WebApi.Infrastructure.Security;
+using Xunit;
+
+namespace Qweree.Authentication.WebApi.Test.Infrastructure.Security
+{
+    public class AuthorizationHeaderEncoderTest
+    {
+        [Fact]
+        public void TestEncodeDecode()
+        {
+            var credentials = new ClientCredentials("id", "password");
+            var encoder = new AuthorizationHeaderEncoder();
+            var hash = encoder.Encode(credentials);
+            var actualCredentials = encoder.Decode(hash);
+
+            actualCredentials.ShouldDeepEqual(credentials);
+        }
+    }
+}
