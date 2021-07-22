a3bb2a16e13a07232a7ff79e322d1ece87db7d36
Tomáš Chroboček <tomas@chrobocek.com>
2021-07-22 22:34:56 +0200

    Added Qweree.ConsoleHost project

---
+++ src/Qweree.ConsoleHost/ConsoleApplicationBuilder.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Qweree.ConsoleHost
+{
+
+    public delegate Task RequestDelegate(ConsoleContext context, CancellationToken cancellationToken = new());
+
+
+    public class ConsoleApplicationBuilder
+    {
+        private readonly List<Func<RequestDelegate?, RequestDelegate>> _delegates = new();
+
+        public ConsoleApplicationBuilder(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        public IServiceProvider ServiceProvider { get; }
+
+        public void Use(Func<RequestDelegate?, RequestDelegate> func)
+        {
+            _delegates.Add(func);
+        }
+
+        public RequestDelegate Build()
+        {
+            return Application;
+        }
+
+        private async Task Application(ConsoleContext context, CancellationToken cancellationToken = new())
+        {
+            RequestDelegate? next = null;
+            for (var i = _delegates.Count - 1; i >= 0; i--)
+            {
+                var func = _delegates[i];
+                next = func(next);
+            }
+
+            if (next != null)
+            {
+                await next(context, cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/Qweree.ConsoleHost/ConsoleContext.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Qweree.ConsoleHost
+{
+    public class ConsoleContext
+    {
+        public string[] Args { get; set; } = Array.Empty<string>();
+        public int? ReturnCode { get; set; } = null;
+    }
+}
