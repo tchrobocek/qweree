5c9c1c93b89821c94585bcaadb86a9f5ec53ee11
Tomáš Chroboček <tomas@chrobocek.com>
2021-08-11 15:30:13 +0200

    Added method to set claims principal within app

--- src/Qweree.WebApplication/Infrastructure/Authentication/ApplicationAuthenticationStateProvider.cs
+++ src/Qweree.WebApplication/Infrastructure/Authentication/ApplicationAuthenticationStateProvider.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -7,26 +6,22 @@ namespace Qweree.WebApplication.Infrastructure.Authentication
 {
     public class ApplicationAuthenticationStateProvider : AuthenticationStateProvider
     {
+        private ClaimsPrincipal? _claimsPrincipal;
+
+        public void SetClaimsPrincipal(ClaimsPrincipal? claimsPrincipal)
+        {
+            _claimsPrincipal = claimsPrincipal;
+        }
+
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var user = new ClaimsPrincipal(CreateAnonymousIdentity());
+            ClaimsPrincipal user = _claimsPrincipal ?? new ClaimsPrincipal(CreateAnonymousIdentity());
             return Task.FromResult(new AuthenticationState(user));
         }
 
         private ClaimsIdentity CreateAnonymousIdentity()
         {
             return new(authenticationType: null);
-        }
-
-        private ClaimsIdentity CreateAuthenticatedIdentity()
-        {
-            return new(new[]
-            {
-                new Claim("userId", Guid.NewGuid().ToString()),
-                new Claim("username", "user"),
-                new Claim("full_name", "user userowitch"),
-                new Claim("email", "user@email.com"),
-            }, "auth-type");
         }
     }
 }
