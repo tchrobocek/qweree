37e5dc39b29517ccb2ae8e502629afcae8a81ca9
Tomáš Chroboček <tomas@chrobocek.com>
2021-08-12 19:51:55 +0200

    Added PiccStash.WebApi project

---
+++ src/Qweree.PiccStash.WebApi/Web/System/HealthReportDto.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Qweree.PiccStash.WebApi.Web.System
+{
+    public class HealthReportDto
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public string? Status { get; set; }
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public Dictionary<string, HealthReportEntryDto>? Entries { get; set; }
+    }
+
+    public class HealthReportEntryDto
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public string? Status { get; set; }
+    }
+}
---
+++ src/Qweree.PiccStash.WebApi/Web/System/VersionDto.cs
@@ -0,0 +1,8 @@
+namespace Qweree.PiccStash.WebApi.Web.System
+{
+    public class VersionDto
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public string? Version { get; set; }
+    }
+}
