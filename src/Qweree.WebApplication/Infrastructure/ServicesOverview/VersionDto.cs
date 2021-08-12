bbe907ea3e8db4c3a17c917bfdfdc879c2b23297
Tomáš Chroboček <tomas@chrobocek.com>
2021-08-12 18:32:21 +0200

    Added system info client

---
+++ src/Qweree.WebApplication/Infrastructure/ServicesOverview/VersionDto.cs
@@ -0,0 +1,8 @@
+namespace Qweree.WebApplication.Infrastructure.ServicesOverview
+{
+    public class VersionDto
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public string? Version { get; set; }
+    }
+}
