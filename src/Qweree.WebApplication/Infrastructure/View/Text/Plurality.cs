c165354d58a55bcd59ffbbbe7da7cc216112d351
Tomáš Chroboček <tomas@chrobocek.com>
2021-09-27 21:46:24 +0200

    Added plurality component

---
+++ src/Qweree.WebApplication/Infrastructure/View/Text/Plurality.cs
@@ -0,0 +1,13 @@
+namespace Qweree.WebApplication.Infrastructure.View.Text
+{
+    public static class Plurality
+    {
+        public static string Decide(int value, string singular, string plural)
+        {
+            if (value == 1)
+                return singular;
+
+            return plural;
+        }
+    }
+}
