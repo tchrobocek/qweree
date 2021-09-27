2c26df437b00f29fb705f3b0caa51f8fd89e4d53
Tomáš Chroboček <tomas@chrobocek.com>
2021-09-27 22:48:31 +0200

    Added current and prev dir to explorer

--- src/Qweree.Cdn.Sdk/Explorer/IExplorerObjectDto.cs
+++ src/Qweree.Cdn.Sdk/Explorer/IExplorerObjectDto.cs
@@ -1,8 +1,12 @@
+using System;
+
 namespace Qweree.Cdn.Sdk.Explorer
 {
     public interface IExplorerObjectDto
     {
         string? Path { get; set; }
+        DateTime? CreatedAt { get; set; }
+        DateTime? ModifiedAt { get; set; }
         string TypeName { get; }
     }
 }
