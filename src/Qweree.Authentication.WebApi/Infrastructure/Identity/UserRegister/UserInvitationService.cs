a917577403ca7c4aefc2961717de8e916913cd83
Tomáš Chroboček <tomas@chrobocek.com>
2021-10-05 00:20:43 +0200

    Added user invitation service with user invite

---
+++ src/Qweree.Authentication.WebApi/Infrastructure/Identity/UserRegister/UserInvitationService.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Qweree.AspNet.Application;
+using Qweree.Authentication.AdminSdk.Identity.Users.UserRegister;
+using Qweree.Authentication.WebApi.Infrastructure.Validations;
+using Qweree.Mongo.Exception;
+using Qweree.Validator;
+
+namespace Qweree.Authentication.WebApi.Infrastructure.Identity.UserRegister
+{
+    public class UserInvitationService
+    {
+        private readonly IValidator _validator;
+        private readonly UserInvitationRepository _userInvitationRepository;
+
+        public UserInvitationService(IValidator validator, UserInvitationRepository userInvitationRepository)
+        {
+            _validator = validator;
+            _userInvitationRepository = userInvitationRepository;
+        }
+
+        public async Task<Response<UserInvitation>> CreateInvitationAsync(UserInvitationInput input, CancellationToken cancellationToken = new())
+        {
+            var invitation = new UserInvitation(Guid.NewGuid(), input.Username, input.FullName, input.ContactEmail,
+                input.Roles);
+
+            var validationResult = await _validator.ValidateAsync(input, cancellationToken);
+            if (validationResult.HasFailed)
+                return validationResult.ToErrorResponse<UserInvitation>();
+
+            try
+            {
+                await _userInvitationRepository.InsertAsync(invitation, cancellationToken);
+            }
+            catch (InsertDocumentException)
+            {
+                return Response.Fail<UserInvitation>("User invitation is a duplicate.");
+            }
+
+            return Response.Ok(invitation);
+        }
+    }
+}
--- src/Qweree.Authentication.WebApi/Infrastructure/ValidationMap.cs
+++ src/Qweree.Authentication.WebApi/Infrastructure/ValidationMap.cs
@@ -1,6 +1,7 @@
 using Qweree.Authentication.AdminSdk.Authorization.Roles;
 using Qweree.Authentication.AdminSdk.Identity.Clients;
 using Qweree.Authentication.AdminSdk.Identity.Users;
+using Qweree.Authentication.AdminSdk.Identity.Users.UserRegister;
 using Qweree.Authentication.Sdk.Account;
 using Qweree.Authentication.WebApi.Infrastructure.Authorization.Roles;
 using Qweree.Authentication.WebApi.Infrastructure.Identity;
@@ -99,6 +100,20 @@ namespace Qweree.Authentication.WebApi.Infrastructure
                 c.AddProperty(p => p.NewPassword)
                     .AddConstraint(new NotEmptyConstraint("Password cannot be empty."))
                     .AddConstraint(new PasswordConstraint());
+            });
+
+            builder.AddModel<UserInvitationInput>(c =>
+            {
+                c.AddProperty(p => p.Username)
+                    .AddConstraint(new UniqueConstraint(typeof(UserRepository)))
+                    .AddConstraint(new NotEmptyConstraint("Username cannot be empty."))
+                    .AddConstraint(new MinLengthConstraint(3, "Username has to have 3 or more characters."))
+                    .AddConstraint(new MaxLengthConstraint(255, "Username has to have up to 255 characters."));
+                c.AddProperty(p => p.ContactEmail)
+                    .AddConstraint(new NotEmptyConstraint("Username cannot be empty."))
+                    .AddConstraint(new EmailConstraint("Email cannot be empty.", "Email is not valid email address."));
+                c.AddProperty(p => p.Roles)
+                    .AddConstraint(new ExistsConstraint(typeof(UserRoleRepository)));
             });
         }
     }
