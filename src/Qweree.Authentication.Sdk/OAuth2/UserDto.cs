b0d103d38ee873f9d842bf7649bc91faed24c832
Tomáš Chroboček <tomas@chrobocek.com>
2021-12-14 21:20:18 +0100

    Added UserDto as equivalent for Claims principal and according mapper

---
+++ src/Qweree.Authentication.Sdk/OAuth2/UserDto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Qweree.Authentication.Sdk.OAuth2
+{
+    public class UserDto
+    {
+        public Guid? Id { get; set; }
+        public string? Username { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string[]? Roles { get; set; }
+    }
+}
