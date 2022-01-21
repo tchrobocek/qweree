09478788f21520f61b2ea18471c599ea28c74ef2
Tomáš Chroboček <tomas@chrobocek.com>
2022-01-21 23:03:52 +0100

    Added system controller to Gateway

---
+++ src/Qweree.Gateway.WebApi/Web/System/HealthReportMapper.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Qweree.Gateway.WebApi.Web.System
+{
+    public static class HealthReportMapper
+    {
+        public static HealthReportDto ToDto(HealthReport report)
+        {
+            return new()
+            {
+                Status = report.Status.ToString(),
+                Entries = report.Entries.ToDictionary(kv => kv.Key, kv => ToDto(kv.Value))
+            };
+        }
+
+        public static HealthReportEntryDto ToDto(HealthReportEntry entry)
+        {
+            return new()
+            {
+                Status = entry.Status.ToString()
+            };
+        }
+    }
+}
