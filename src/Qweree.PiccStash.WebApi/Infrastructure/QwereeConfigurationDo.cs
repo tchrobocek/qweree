1a27514c62b32f43d191fe1b783fca34e8b2b545
Tomáš Chroboček <tomas@chrobocek.com>
2022-02-12 01:58:50 +0100

    Unified configuration -- picc

--- src/Qweree.PiccStash.WebApi/Infrastructure/QwereeConfigurationDo.cs
+++ src/Qweree.PiccStash.WebApi/Infrastructure/QwereeConfigurationDo.cs
@@ -2,6 +2,14 @@ namespace Qweree.PiccStash.WebApi.Infrastructure;
 
 public class QwereeConfigurationDo
 {
+    public string? MongoConnectionString { get; set; }
+    public string? HealthCheckConnectionString { get; set; }
+    public string? DatabaseName { get; set; }
+    public string? AccessTokenKey { get; set; }
+    public string? PathBase { get; set; }
+    public string? SwaggerTokenUri { get; set; }
+    public string? AuthTokenUri { get; set; }
+    public string? CdnUri { get; set; }
     public string? ClientId { get; set; }
     public string? ClientSecret { get; set; }
 }
