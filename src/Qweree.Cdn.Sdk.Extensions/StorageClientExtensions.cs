441b861f70ec8d8704360a7b4c43b2b640c491c9
Tomáš Chroboček <tomas@chrobocek.com>
2022-02-12 16:55:37 +0100

    Added storage client extensions to retrieve and store objects in the same fasion

---
+++ src/Qweree.Cdn.Sdk.Extensions/StorageClientExtensions.cs
@@ -0,0 +1,23 @@
+using System.Net.Mime;
+using Qweree.Cdn.Sdk.Storage;
+using Qweree.Utils;
+
+namespace Qweree.Cdn.Sdk.Extensions;
+
+public static class StorageClientExtensions
+{
+    public static async Task<TResultType?> RetrieveAsObjectAsync<TResultType>(this StorageClient @this, string path,  CancellationToken cancellationToken = new()) where TResultType : class
+    {
+        using var response = await @this.RetrieveAsync(path, cancellationToken);
+        response.EnsureSuccessStatusCode();
+        await using var stream = await response.ReadPayloadAsStreamAsync(cancellationToken);
+        return await JsonUtils.DeserializeAsync<TResultType>(stream, cancellationToken);
+    }
+    public static async Task StoreAsObjectAsync(this StorageClient @this, string path, object value, bool force = false, bool isPrivateResource = true,  CancellationToken cancellationToken = new())
+    {
+        await using var stream = new MemoryStream();
+        await JsonUtils.SerializeAsync(stream, value, cancellationToken);
+        using var response = await @this.StoreAsync(path, MediaTypeNames.Application.Json, stream, force, isPrivateResource, cancellationToken);
+        response.EnsureSuccessStatusCode();
+    }
+}
