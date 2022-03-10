739fefa13fda00444c45f6bb6eecf974d258785e
Tomáš Chroboček <tomas@chrobocek.com>
2022-03-10 21:17:54 +0100

    Added option to turn off swagger link

--- src/Qweree.WebApplication/Infrastructure/ServicesOverview/ServiceInfo.cs
+++ src/Qweree.WebApplication/Infrastructure/ServicesOverview/ServiceInfo.cs
@@ -4,7 +4,7 @@ public class ServiceInfo
 {
     public string Label { get; set; } = string.Empty;
     public string Uri { get; set; } = string.Empty;
-    public string SwaggerUri { get; set; } = string.Empty;
+    public string? SwaggerUri { get; set; }
     public string Version { get; set; } = "0.0.0";
     public HealthReportDto HealthReport { get; set; } = new();
 }
